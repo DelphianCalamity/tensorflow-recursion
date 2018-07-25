cea3b05745eb1b78b81b0faa4214bb895f1923c2
Angelos Charalambidis <agcharal@gmail.com>
2018-07-25 12:20:53 +0300

    WIP again

--- tensorflow/core/grappler/optimizers/function_transformation.cc
+++ tensorflow/core/grappler/optimizers/function_transformation.cc
@@ -193,13 +193,14 @@ struct CallInfo {
     std::unordered_map<string, AttrValue> attr;
 };
 
-Status GatherCalls(const GrapplerItem& item, const FunctionInliningContext& ctx,
+Status GatherCalls(GraphDef& graph,
+                   const FunctionInliningContext& ctx,
                    std::unordered_map<string,CallInfo>& calls) {
     std::unordered_map<string, std::pair<int,string>> out_to_node;
     int id = 1;
 
     // identify and collect calls in the graph
-    for (const NodeDef& node : item.graph.node()) {
+    for (const NodeDef& node : graph.node()) {
         const FunctionDef* func = ctx.FindInlinedFunction(node.op());
         if (func != nullptr) {
             CallInfo& call = calls[node.name()];
@@ -361,54 +362,64 @@ Status ConnectInput(NodeDef* from, NodeDef* to) {
     return Status::OK();
 }
 
-Status TransformCall(const CallInfo& call_info, FunctionInliningContext& ctx,
-                     GraphDef* optimized_graph) {
+Status TransformCall(CallInfo& call_info,
+                     FunctionInliningContext& ctx,
+                     GraphDef* graph) {
     FuncInfo func_info;
 
     // inlines the body of a function and provides a struct with func_info
     TF_RETURN_IF_ERROR(ctx.FindCompatibleOrInlineFunction(
-        call_info.function_name, call_info.attr, optimized_graph, func_info));
+        call_info.function_name, call_info.attr, graph, func_info));
 
     CHECK_EQ(call_info.input_nodes.size(), func_info.inputs.size());
 
-    std::vector<NodeDef*> calls;
-    std::vector<NodeDef*> returns;
+    std::vector<NodeDef*> call_nodes;
+    std::vector<NodeDef*> ret_nodes;
 
-    calls.resize(func_info.inputs.size());
+    call_nodes.resize(func_info.inputs.size());
     for (int arg_num; arg_num < call_info.input_nodes.size(); arg_num++) {
-        calls[arg_num] = optimized_graph->add_node();
+        call_nodes[arg_num] = graph->add_node();
         AddCall(call_info,
                 func_info.input_def[arg_num],
                 call_info.input_nodes[arg_num],
                 arg_num,
-                calls[arg_num]);
+                call_nodes[arg_num]);
 
         // connect the input of the inlined function to feed from call.
-        TF_RETURN_IF_ERROR(ConnectInput(calls[arg_num], func_info.inputs[arg_num]));
+        TF_RETURN_IF_ERROR(ConnectInput(call_nodes[arg_num], func_info.inputs[arg_num]));
     }
 
     for (std::pair<int,NodeInputDescriptor> out_entry : call_info.output_nodes) {
         int out_port = out_entry.first;
-        NodeDef* ret = optimized_graph->add_node();
+        NodeDef* ret = graph->add_node();
         AddRet(call_info,
                func_info.output_def[out_port],
                func_info.outputs[out_port],
                out_port,
                ret);
-        returns.push_back(ret);
+        ret_nodes.push_back(ret);
 
         // connect the outputs to feed from returns.
     }
 
-    // we need to keep track of the added calls and returns
+    // for each call create a control dependency to each return
+    // to facilitate dead propagation semantics
+
+    // remove the original call
+    // set it to noop in order to be garbage collected latter
+
+    // call_info.node.clear_input();
+    // call_info.node.set_op("NoOp");
+    // call_info.node.set_name(strings::StrCat("_noop_", call_info.node_name));
 
     return Status::OK();
 }
 
 
-Status InlineFunction(const FunctionDef& func_def, const FunctionInliningContext& ctx,
+Status InlineFunction(const FunctionDef& func_def,
+                      const FunctionInliningContext& ctx,
                       const std::unordered_map<string, AttrValue>& func_attr,
-                      GraphDef* optimized_graph, FuncInfo& func_info) {
+                      GraphDef* graph, FuncInfo& func_info) {
 
     std::unique_ptr<GrapplerItem> item = GrapplerItemFromFunctionDef(func_def, func_attr, ctx.Library());
 
@@ -419,64 +430,66 @@ Status InlineFunction(const FunctionDef& func_def, const FunctionInliningContext
                 "Failed to inline function ", func_def.signature().name());
     }
 
-    func_info.fetch = item->fetch;
-    func_info.outputs = item->fetch;
-    ArgMergeMap& argmerge_map = func_info.argMergeMap;
-
     std::unordered_map<string, int> input_nodes;
     for (int i = 0; i < func_def.signature().input_arg_size(); ++i) {
       const OpDef::ArgDef& arg = func_def.signature().input_arg(i);
       input_nodes[arg.name()] = i;
 
       // Create and add a temporary merge node (IdentityN) for every input arg
-      NodeDef* merge = optimized_graph->add_node();
-      merge->set_name(strings::StrCat(prefix, "/", "Merge_", i));
+      NodeDef* merge = graph->add_node();
+      merge->set_name(AddPrefixToNodeName(strings::StrCat("Merge_", i), prefix));
       merge->set_op("Identity");
       func_info.inputs[i] = merge;
       func_info.input_def[i] = arg;
-      argmerge_map.emplace(arg.name(), merge);
     }
 
     for (NodeDef& func_body_node : *item->graph.mutable_node()) {
+      const string& curr_name = func_body_node.name();
       // If the func body node is func's input argument
-      if (input_nodes.find(func_body_node.name()) != input_nodes.end()) {
+      auto input_it = input_nodes.find(curr_name);
+
+      if (input_it != input_nodes.end()) {
         CHECK_EQ(0, func_body_node.input_size());
         // Turn input placeholders into identity nodes
         if (IsPlaceholder(func_body_node)) {
           func_body_node.set_op("Identity");
         }
         // Connect merge with input arg
-        func_body_node.add_input(argmerge_map[func_body_node.name()]->name());
+        func_body_node.add_input(func_info.inputs[*input_it]->name());
       } else { // Else if not an input_arg_node
         // Update the input names if any.
         for (string& input : *func_body_node.mutable_input()) {
-          input = AddPrefixToNodeName(input, /*prefix=*/prefix);
+          input = AddPrefixToNodeName(input, prefix);
         }
         // If the node has no input, make hook it up to the Merge nodes to ensure
         // it runs in the same frame as the other nodes of the function body.
         if (func_body_node.input_size() == 0) {
-          for (auto it = argmerge_map.begin(); it != argmerge_map.end(); ++it) {
-            *func_body_node.add_input() = AsControlDependency(it->second->name());
+          for (auto& func_input_node : func_info.inputs) {
+            *func_body_node.add_input() = AsControlDependency(func_input_node.name());
           }
         }
       }
 
       // Add the node name as a prefix to avoid collisions after inlining
-      func_body_node.set_name(strings::StrCat(prefix, "/", func_body_node.name()));
+      func_body_node.set_name(AddPrefixToNodeName(curr_name, prefix));
 
       // Make sure the node is placed
       //func_body_node.set_device(func_node.device());
 
       // Move the node to the main graph
-      optimized_graph->add_node()->Swap(&func_body_node);
+      graph->add_node()->Swap(&func_body_node);
     }
+
+    // Record the outputs of the function (possibly need to be prefixed).
+    func_info.outputs = item->fetch;
+
     return Status::OK();
 }
 
 Status FunctionInliningContext::FindCompatibleOrInlineFunction(
-            const string& function_name,
+            const string& func_name,
             const std::unordered_map<string, AttrValue>& func_attr,
-            GraphDef* optimized_graph,
+            GraphDef* graph,
             FuncInfo& func_info) {
     const auto& it = transformed_functions_.find(function_name);
 
@@ -487,7 +500,7 @@ Status FunctionInliningContext::FindCompatibleOrInlineFunction(
         return Status::OK();
     }
 
-    const FunctionDef* func_def = FindInlinedFunction(function_name);
+    const FunctionDef* func_def = FindInlinedFunction(func_name);
 
     if (func_def == nullptr) {
         return errors::InvalidArgument(
@@ -495,9 +508,10 @@ Status FunctionInliningContext::FindCompatibleOrInlineFunction(
                         "or not marked to be inlined");
     }
 
-    TF_RETURN_IF_ERROR(InlineFunction(*func_def, *this, func_attr, optimized_graph, func_info));
+    TF_RETURN_IF_ERROR(
+        InlineFunction(*func_def, *this, func_attr, graph, func_info));
 
-    transformed_functions_[function_name] = func_info;
+    transformed_functions_[func_name] = func_info;
 
     return Status::OK();
 }
